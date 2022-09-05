24f832bada53c43d92a27e239018e814700ee3cf
Alessandra Pereira <alessandrapereira@outlook.com>
2022-09-05 17:16:37 -0300

    Onde parou na segunda de manhã

--- AdeusStoryboard/ViewController.swift
+++ AdeusStoryboard/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -16,6 +16,76 @@ class ViewController: UIViewController {
         view.backgroundColor = .blue
     }
     
+    override func loadView() {
+        // Inicialização da view que irá ser atribuída como view raiz do view controller
+        let view = UIView()
+        
+        // MARK: - Inicialização dos componentes
+        let backgroundView = UIImageView(image: UIImage(named: "background"))
+        let logoView = UIImageView()
+        let diceView = UIImageView()
+        let playButton = UIButton(configuration: .filled())
+        
+        let logoContainer = UIView()
+        let diceContainer = UIView()
+        let playButtonContainer = UIView()
+        
+        //        let stackView = UIStackView(arrangedSubviews: [logoContainer, diceContainer, playButtonContainer])
+        let stackView = UIStackView()
+        
+        // MARK: - Setup na hierarquia das views
+        view.addSubview(backgroundView)
+        view.addSubview(stackView)
+        
+        stackView.addArrangedSubview(logoContainer)
+        stackView.addArrangedSubview(diceContainer)
+        stackView.addArrangedSubview(playButtonContainer)
+        
+        logoContainer.addSubview(logoView)
+        diceContainer.addSubview(diceView)
+        playButtonContainer.addSubview(playButton)
+        
+        
+        // MARK: - Configuração dos atributos dos componentes
+        backgroundView.contentMode = .scaleAspectFill
+        
+        logoView.image = UIImage(named: "logo-jogada")
+        diceView.image = UIImage(named: "dice-1")
+        
+        stackView.axis = .vertical
+        stackView.alignment = .fill
+        stackView.distribution = .fillEqually
+        stackView.spacing = 32
+        
+        
+        // MARK: - Configuração das constraints de auto layout
+        backgroundView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            backgroundView.topAnchor.constraint(equalTo: view.topAnchor),
+            backgroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            backgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            backgroundView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 16),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
+        logoView.translatesAutoresizingMaskIntoConstraints = false
+        
+        diceView.translatesAutoresizingMaskIntoConstraints = false
+        
+        playButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        
+        // Atribuição da view inicializada no começo da função
+        self.view = view
+    }
     
 }
 
@@ -28,8 +98,8 @@ struct ViewController_Preview: PreviewProvider {
     static var previews: some View {
         // view controller using programmatic UI
         Group {
-            ViewController().showPreview().previewDevice("iPhone 13")
-            ViewController().showPreview().previewDevice("iPhone 13").previewInterfaceOrientation(.landscapeLeft)
+            ViewController().showPreview().previewDevice("iPhone SE (3rd generation)")
+            ViewController().showPreview().previewDevice("iPhone SE (3rd generation)").previewInterfaceOrientation(.landscapeLeft)
         }
     }
 }
